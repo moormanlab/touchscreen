7501d4fe1d9ce8f0a66ccb8f4ffad40d5b83f964
abigailrgray <63576437+abigailrgray@users.noreply.github.com>
2021-04-07 01:53:33 -0400

    moved return_to_menu function into a separate file

--- mouse_touchscreen.py
+++ mouse_touchscreen.py
@@ -12,6 +12,7 @@ import logging
 import io
 import os
 from hal import Buzzer, IRSensor, Valve, isRaspberryPI
+from return_to_menu import return_to_menu
 #colors
 red = (255,0,0)
 green = (0,255,0)
@@ -308,38 +309,6 @@ def classicalConditioning(screen):
     # Done! Time to quit.
     return
 
-
-def return_to_menu(event,screen):
-    running = True
-    if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.FINGERDOWN:
-        # Draws invisible box in top right corner to return to menu
-        closing_box = pygame.draw.rect(screen, black, (700,0, 100,100))
-        # Checks if screen was pressed in top right corner
-        curs_in_box = closing_box.collidepoint(pygame.mouse.get_pos())
-        # Time elapsed since corner was pressed
-        startTime = time.time()
-        elapsed = 0
-        # Checks time elapsed every second and closes program once 2 seconds have passed
-        while curs_in_box:
-            if elapsed > 2:
-                return False
-            # If corner is no longer being pressed, the loop breaks
-            elif mouse_event_type() == False:
-                break
-            time.sleep(1)
-            elapsed = time.time() - startTime
-            logging.info('Top right corner pressed. Number of seconds elapsed: {:.2f}'.format(elapsed))
-        
-    return running
-
-# Checks that corner is still pressed while time is <2 sec
-def mouse_event_type():
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN or event.type == 32779 or event.type == pygame.FINGERDOWN:
-            return True
-        else:
-            return False
-        
 
 if __name__ == '__main__':
     pygame.init()
