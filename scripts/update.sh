d798e1ad350f30ff687ed2fc90c6d19eedbc7b21
the-drummist <adkramer@umass.edu>
2022-07-14 16:30:03 -0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 source /home/pi/touchenv/bin/activate
 cd /home/pi/touchscreen/
 git reset --hard HEAD
-git checkout main
+git checkout client
 git pull
 pip install --upgrade pip setuptools
 pip install --upgrade -r /home/pi/touchscreen/requirements.txt
